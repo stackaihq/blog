aec468a611fcd0e4ccd974d1115a26682565dc3e
abishek-trainn <abishek@trainn.co>
2023-02-25 20:01:18 +0530

    wip: added /blog support

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-paper.pages.dev/",
+  base: "/blog",
+  trailingSlash: "never",
   integrations: [
     tailwind({
       config: {
